b10be69b58b7bde04017db40c199d53ec98432f5
isaac castro <isaacokechukwu20021@gmail.com>
2023-12-07 22:58:17 +0100

    testing

--- 0x00-python_variable_annotations/100-safe_first_element.py
+++ 0x00-python_variable_annotations/100-safe_first_element.py
@@ -1,11 +1,9 @@
 #!/usr/bin/env python3
 """Duck Tying Annotation"""
 from typing import Sequence, Any, Union
-import types
-
 
 # The types of the elements of the input are not know
-def safe_first_element(lst: Sequence[Any]) -> Union[Any, types.NoneType]:
+def safe_first_element(lst: Sequence[Any]) -> Union[Any, None]:
     """safe_first_element:
 takes lst: Sequence[Any] as argument and return a union type"""
     if lst:
