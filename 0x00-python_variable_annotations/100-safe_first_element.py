7dc6ba0ee1f0284a7f16eece3d4017ae55f42132
isaac castro <isaacokechukwu20021@gmail.com>
2023-12-07 23:00:55 +0100

    fix

--- 0x00-python_variable_annotations/100-safe_first_element.py
+++ 0x00-python_variable_annotations/100-safe_first_element.py
@@ -2,6 +2,7 @@
 """Duck Tying Annotation"""
 from typing import Sequence, Any, Union
 
+
 # The types of the elements of the input are not know
 def safe_first_element(lst: Sequence[Any]) -> Union[Any, None]:
     """safe_first_element:
