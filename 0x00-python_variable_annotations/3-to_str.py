71640923c3c1b9d594eb80c7c7aa7d4b5c85fe12
isaac castro <isaacokechukwu20021@gmail.com>
2023-12-07 21:22:22 +0100

    3. Basic annotations - to string

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+"""Type annotation function"""
+
+
+def to_str(n: float) -> str:
+    """takes a float n and return the string representation of the float"""
+    return str(n)
