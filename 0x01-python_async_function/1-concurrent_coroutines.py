ae6334c9f269372cc7500fd808d507a45e7184e0
Isaac Okechukwu Chukwuma <isaacokechukwu200021@gmail.com>
2023-12-12 17:04:02 +0100

    Fix import statement in basic_async_syntax.py

---
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""Let's execute multiple coroutines at the same time with async"""
+
+import asyncio
+import random
+from typing import List
+
+wait_random = __import__('0-basic_async_syntax').wait_random
+
+
+async def wait_n(n: int = 0, max_delay: int = 10) -> List[float]:
+    """spawn wait_random n times with the specified max_delay."""
+    list_float = []
+    for i in range(n):
+        list_float.append(asyncio.create_task(wait_random(max_delay)))
+    return [await task for task in asyncio.as_completed(list_float)]
