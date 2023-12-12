5227781d8e602d918e14e9d63917db89dd4df855
Isaac Okechukwu Chukwuma <isaacokechukwu200021@gmail.com>
2023-12-12 17:38:27 +0100

    Refactor task_wait_n function to return a list of delays

--- 0x01-python_async_function/4-tasks.py
+++ 0x01-python_async_function/4-tasks.py
@@ -2,14 +2,16 @@
 """Tasks"""
 import asyncio
 import random
+from typing import List
 
 
 wait_random = __import__('0-basic_async_syntax').wait_random
+task_wait_random = __import__('3-tasks').task_wait_random
 
 
-async def task_wait_n(n: int = 0, max_delay: int = 10) -> asyncio.Task:
-    """returns a asyncio.Task"""
+async def task_wait_n(n: int, max_delay: int) -> List[float]:
+    """returns a list of all the delays"""
     list_float = []
     for i in range(n):
-        list_float.append(asyncio.create_task(wait_random(max_delay)))
+        list_float.append(task_wait_random(max_delay))
     return [await task for task in asyncio.as_completed(list_float)]
